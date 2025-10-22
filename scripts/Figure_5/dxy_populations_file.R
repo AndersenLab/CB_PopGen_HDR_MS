6116916dff117c9629ba14f772d3efe6285487f1
Bowen Wang <111165149+Bowenw6@users.noreply.github.com>
2025-10-22 14:14:14 -0400

    Add files via upload

---
+++ scripts/Figure_5/dxy_populations_file.R
@@ -0,0 +1,68 @@
+
+rm(list=ls())
+
+library(readr)
+library(dplyr)
+
+lineage<- read_tsv("../../data/From_Nic/isotype_byLineage_GeoLocAdmCol_20250909.tsv") %>% 
+  select(isotype,Lineage)
+
+
+Tropical_AD<-lineage %>% 
+  filter(Lineage %in% c("Tropical","AD"))
+
+Tropical_KD<-lineage %>% 
+  filter(Lineage %in% c("Tropical","KD"))
+
+Tropical_TD1<-lineage %>% 
+  filter(Lineage %in% c("Tropical","TD1"))
+
+Tropical_Temperate<-lineage %>% 
+  filter(Lineage %in% c("Tropical","Temperate"))
+
+Tropical_TH<-lineage %>% 
+  filter(Lineage %in% c("Tropical","TH"))
+
+
+
+
+write.table(Tropical_AD,"../../processed_data/Dxy_Tropical/Tropical_AD.txt",
+            quote = FALSE,  
+            row.names = FALSE,
+            col.names = FALSE,
+            sep = '\t')
+
+
+
+write.table(Tropical_KD,"../../processed_data/Dxy_Tropical/Tropical_KD.txt",
+            quote = FALSE,  
+            row.names = FALSE,
+            col.names = FALSE,
+            sep = '\t')
+
+
+
+write.table(Tropical_TD1,"../../processed_data/Dxy_Tropical/Tropical_TD1.txt",
+            quote = FALSE,  
+            row.names = FALSE,
+            col.names = FALSE,
+            sep = '\t')
+
+
+write.table(Tropical_Temperate,"../../processed_data/Dxy_Tropical/Tropical_Temperate.txt",
+            quote = FALSE,  
+            row.names = FALSE,
+            col.names = FALSE,
+            sep = '\t')
+
+
+
+
+write.table(Tropical_TH,"../../processed_data/Dxy_Tropical/Tropical_TH.txt",
+            quote = FALSE,  
+            row.names = FALSE,
+            col.names = FALSE,
+            sep = '\t')
+
+
+
